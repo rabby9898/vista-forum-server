a6f797720bec5820b3aa6607ecba5bfe50c19d0d
rabby9898 <mdfajlerabby00@gmail.com>
2023-12-04 22:51:50 +0600

    final deploy

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const announcementCollection = client
       .db("vistaForum")
       .collection("announcement");
@@ -88,7 +88,10 @@ async function run() {
     });
     // all post get method
     app.get("/posts", async (req, res) => {
-      const result = await addPostsCollection.find().toArray();
+      const result = await addPostsCollection
+        .find()
+        .sort({ posted_time: -1 })
+        .toArray();
       res.send(result);
     });
     app.get("/allPosts", async (req, res) => {
@@ -160,10 +163,10 @@ async function run() {
       res.send(result);
     });
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
