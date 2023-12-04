64527327df13978764a984edfc573be222e86e71
rabby9898 <mdfajlerabby00@gmail.com>
2023-12-04 21:45:33 +0600

    admin verify added

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function run() {
         return res.status(401).send({ message: "forbidden access" });
       }
       const token = req.headers.authorization.split(" ")[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_JWT_SECRET, (err, decoded) => {
+      jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
         if (err) {
           return res.status(401).send({ message: "forbidden access" });
         }
@@ -45,10 +45,23 @@ async function run() {
         next();
       });
     };
+
+    // verify admin
+    // const verifyAdmin = async (req, res, next) => {
+    //   const email = req.decoded.email;
+    //   const query = { email: email };
+    //   const user = await usersCollection.findOne(query);
+    //   const isAdmin = user?.role == "admin";
+    //   if (!isAdmin) {
+    //     return res.status(403).send({ message: "Unauthorized access" });
+    //   }
+    //   next();
+    // };
+
     // jwt api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_JWT_SECRET, {
+      const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, {
         expiresIn: "1h",
       });
       res.send({ token });
@@ -113,18 +126,33 @@ async function run() {
       const result = await addPostsCollection.deleteOne(query);
       res.send(result);
     });
-    // admin api
-    app.patch("/users/admin/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updatedDoc = {
-        $set: {
-          role: "admin",
-        },
-      };
-      const result = await usersCollection.updateOne(filter, updatedDoc);
-      res.send(result);
+    // admin role apis
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      // if (email !== req.decoded.email) {
+      //   console.log("admin:false");
+      // }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
     });
+    // // admin api
+    // app.patch("/users/admin/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const updatedDoc = {
+    //     $set: {
+    //       role: "admin",
+    //     },
+    //   };
+    //   const result = await usersCollection.updateOne(filter, updatedDoc);
+    //   res.send(result);
+    // });
     // make announcement api
     app.post("/makeAnnounce", async (req, res) => {
       const announce = req.body;
